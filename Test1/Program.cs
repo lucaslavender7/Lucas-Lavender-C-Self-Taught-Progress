ee6c8be618ca8e80303fac05cbad2c5254926447
Lucas Lavender <lucaslavender7@gmail.com>
2020-11-25 00:37:19 -0500

    Add project files.

---
+++ Test1/Program.cs
@@ -0,0 +1,513 @@
+﻿#include <iostream> 
+
+std::cout; 
+
+int main()
+{
+    int slices;
+    slices = 5 + 1;
+    cout << slices; 
+
+}
+
+# include <iostream>
+
+std::cout;
+
+int main()
+
+{
+    int slices;
+    slices = 5 + 1;
+    cout << slices;
+
+}
+
+#include <iostream>
+
+std::cout;
+
+int main()
+
+{
+    int slices
+    slices = 5 + 1;
+    cout << slices;
+
+}
+
+#include <iostream>
+
+std::cout;
+
+int main()
+
+{
+    int slices 
+    slices 5 + 1;
+    cout << slices;
+
+}
+
+#include <iostream>
+
+std::cout;
+
+int main()
+
+{
+    int slices 
+    slices 5 + 1;
+    cout slices;
+
+}
+
+#include <iostream>
+
+std:: cout:
+
+    int main()
+
+{
+    int slices
+    slices = 5 + 1;
+    cout << slices
+
+}
+
+#include <iostream>
+
+std::cout;
+
+int main()
+
+{
+    int slices;
+    slices = 5 + 1;
+    cout << slices; 
+
+}
+
+#include <iostream>
+
+std::cout;
+
+int main()
+
+{
+    int slices;
+    slices = 5 + 1;
+    cout << slices;
+
+}
+
+#include <iostream>
+
+std::cout;
+
+int main()
+
+{
+    int slices;
+    slices = 5 + 1;
+    cout << slices;
+        
+        }
+
+#include <iostream>
+
+std::cout;
+
+int main()
+
+{
+    int slices;
+    slices = 5 + 1;
+    cout << slices;
+
+
+}
+
+#include <iostream>
+
+std::cout;
+
+int main()
+
+{
+    int slices;
+    slices = 5 + 1;
+    cout << slices;
+
+
+}
+
+#include <iostream>
+
+std::cout;
+
+int main()
+
+{
+    int slices;
+    slices = 5 + 1;
+    cout << slices;
+
+
+}
+
+#include <iostream>
+
+std::cout;
+
+int main()
+
+{
+    int slices;
+    slices = 5 + 1;
+    cout << slices;
+}
+
+#include <ionstream>
+
+std::cout;
+
+int main ()
+
+{
+    int slices;
+    slices = 5 + 1;
+    cout << slices;
+
+}
+
+#include <iostream>
+
+using namespace std;
+
+    int main()
+
+{
+    int slices;
+    slices = 5 + 1;
+    cout << slices;
+}
+
+#include <iostream>
+
+std::cout;
+
+    int main()
+{
+    int slices = 10;
+    cout << " You Have " << slices << " slices of pizza." << std::endl;
+
+}
+
+#include <iostream> 
+
+using std:: cout;
+
+    int main;
+
+{
+int slices = 10;
+cout << " You Have " slices >> " slices of pizza." >> std::endl; 
+
+    }
+#include <iostream>
+
+using std::cout;
+
+int main();
+
+{
+    int slices = 10;
+    cout >> " You Have " slices >> " slices of pizza." >> std::endl; 
+
+}
+
+#include <iostream>
+
+using std:: cout;
+
+int main()
+
+{
+    int slices = 10;
+    cout >> " You Have " slices >> " Slices of Pizza." >> std::endl;
+
+}
+
+#include <iostream> 
+
+using std:: cout;
+using std:: cin; 
+
+int main()
+
+{
+    int slices;
+    cout << "Yo how many pieces of pizza do you eat?"
+    cin >> slices;
+    cout << " You Have " slices " Slices of Pizza." << std::endl; 
+}
+
+#include <iostream>
+
+using std:: cout;
+using std:: cin;
+
+int main()
+
+{
+    int slices;
+    cout << "Yo how many peices of pizza do you eat?"
+    cin >> slices;
+    cout << " You Have " slices << " Slices of Pizza." << std::endl;
+
+}
+
+# include <iostream> 
+# include <cmath>
+
+using std:: cout;
+using std:: cin;
+
+int main()
+
+{
+    cout << " What is the base?";
+    cin >> base;
+    cout << " What is the exponent?";
+    cin << exponent;
+    cout << pow(base, exponent); 
+
+}
+
+#include <iostream> 
+#include <cmath>
+
+using std:: cout;
+using std:: cin;
+
+int main()
+
+{
+    cout << " What is the base?";
+    cin >> base;
+    cout << "What is the exponent?";
+    cin >> exponent;
+    cout << pow(base, exponent);
+
+}
+
+#include <iostream>
+#include <cmath>
+
+using std:: cout;
+using std:: cin; 
+
+main int()
+
+    {
+    cout << " What is the base?";
+    cin >> base;
+    cout << " What is the exponent?";
+    cin >> exponent;
+    double power = pow(base, exponent);
+    cout << power << std::endl; 
+}
+
+
+#include <iostream>
+#include <cmath>
+
+using std:: cout;
+using std:: cin;
+
+int main()
+
+{
+    cout << " What is the base?";
+    cin >> base;
+    cout << " What is the exponent?";
+    cin << exponent;
+    cout << pow(base, exponent);
+
+}
+
+#include <iostream>
+#include <cmath>
+
+using std:: cout;
+using std:: cin;
+
+int main()
+
+
+{
+    cout << " What is the base?";
+    cin >> base;
+    cout << " What is the exponen?";
+    cin >> exponent;
+    double power = pow(base, exponent);
+    cout << power << std::endl;
+
+
+
+}
+
+#include <iostream> 
+#include <cmath>
+
+using std:: cout;
+using std:: cin;
+
+double power (double base, int exponent)
+{
+    double result = 1;
+    for (int i = 0; < exponent; i++)
+    {
+        result = result x base;
+    }
+    return result;
+}
+
+int main()
+
+{
+    cout << " What is the base";
+    cin >> base;
+    cout << " What is the exponenet";
+    cin << exponent;
+    double power = pow(base, exponenet);
+    cout << power << std::endl;
+
+}
+
+#include <iostream>
+#include <cmath>
+
+using std:: cout;
+using std:: cin;
+
+double power( double base, int exponent);
+{
+    for (int i = 0; i < exponent; i++) ;
+    {
+        result =result x base;
+    }
+    return result; 
+}
+
+#include <iostream>
+#include <cmath>
+
+using std:: cout;
+using std:: cin;
+
+double power = ( double base, int exponent);
+{
+    double result = 1;
+    for (int i = 0; < exponent; i++);
+    {
+        result = result x base;
+    }
+    return result;
+}
+
+#include <iostream>
+#include <cmath>
+
+using std:: cout;
+using std:: cin;
+
+double power = ( double base, int exponent);
+{
+    double result = 1;
+    for (int i = 0; i < exponent; i++) ;
+    {
+        result = result x base;
+    }
+    return result;
+}
+
+#include <iostream>
+#include <cmath>
+
+using std:: cout;
+using std:: cin;
+
+{
+    double power = (double base,int exponent);
+    double result = 1;
+    for(int i = 0; i < exponent; i++);
+    {
+        result = result x base;
+    }
+    return result;
+}
+
+/* LEARNING HOW TO APPLY FUNCTIONS (REPETITIONS) 
+ 
+double power = ( double base,int exponent);
+for(int i = 0; i < exponent; i++);
+
+double power = (double base, int exponent);
+for(int = 0; i < exponent; int++) ;
+
+double power = ( double base,int exponent);
+for(int i = 0; i < exponent; i++);
+
+double power = ( double base,int exponent);
+for(int i = 0; i < exponent; i++) ;
+
+double power = ( double base, int exponent);
+for(int i = 0; i < exponent; i++)
+
+
+
+
+/* USING POW AS A FUNCTION 
+
+# include <iostream> 
+# include <cmath>
+
+using std:: cout;
+using std:: cin; 
+
+int main()
+
+{
+    cout << " What is the base?";
+    cin >> base;
+    cout << " What is the exponent?";
+    cin >> exponent;
+    cout << pow(base, exponent); 
+
+}
+
+
+ASSIGNING POW TO A VARIABLE
+
+# include <iostream>
+# include <cmath>
+
+
+using std:: cout;
+using std:: cin;
+
+int main()
+
+{
+    cout << " What is the base?";
+    cin >> base;
+    cout << "what is the exponent?";
+    cin >> exponent;
+    double power = pow(base, exponent);
+    cout << power << std::endl; 
+
+}
+
+
